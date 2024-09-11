1e209f908c284ec80937ada4cf89b05971e5f870
AAA-Lin <130432689+AAA-Lin@users.noreply.github.com>
2024-09-12 06:49:39 +0800

    增加余弦相似度计算

--- 3222004851/main.py
+++ 3222004851/main.py
@@ -1,3 +1,21 @@
+from jieba import lcut
+from re import match
+from sys import argv
+from gensim.corpora import Dictionary
+from gensim.similarities import Similarity
+
+
+# 通过该方法实现计算fileStr1和fileStr2的余弦相似度（新增加）
+def calculateSimilarity(fileStr1, fileStr2):
+    texts = [fileStr1, fileStr2]
+    dictionary = Dictionary(texts)
+    corpus = [dictionary.doc2bow(text) for text in texts]
+    similarity = Similarity('-Similarity-index', corpus, num_features=len(dictionary))
+    testCorpus = dictionary.doc2bow(fileStr1)
+    cosineSimilarity = similarity[testCorpus][1]
+    return cosineSimilarity
+
+
 # 从filePath中读取内容
 def getFileContents(filePath):
     ret = ''
@@ -29,6 +47,6 @@ if __name__ == '__main__':
     orig, orig_add, ans = argv[1], argv[2], argv[3]
     print(orig, orig_add, ans)
     similarity = calculateSimilarity(processFile(getFileContents(orig)), processFile(getFileContents(orig_add)))
-    print("文章相似度：%.2f" % (similarity))
+    print("文章相似度为：%.2f" % (similarity))
     with open(ans, 'w', encoding='utf-8') as f:
-        f.write("文章相似度：%.2f" % (similarity))
+        f.write("文章相似度为：%.2f" % (similarity))
